3ffbf5313c3125f3115411eadadfb0d700e8b7cd
Mac <MacXY@herr-der-mails.de>
2022-04-30 10:54:21 +0200

    cleanup: allow customized folders to delete

--- cleanup.js
+++ cleanup.js
@@ -1,5 +1,25 @@
+let options;
+const argsSchema = [
+    ['folder', '/Temp/'],    
+];
+
+export function autocomplete(data, args) {
+	data.flags(argsSchema);
+	const lastFlag = args.length > 1 ? args[args.length - 2] : null;
+    if (["--folder"].includes(lastFlag))
+        return data.scripts;
+	return [];
+}
+
 /** @param {NS} ns **/
 export async function main(ns) {
-    for (let file of ns.ls('home', '/Temp/'))
+    options = ns.flags(argsSchema);
+    if (options.folder && !options.folder.startsWith('/'))
+        options.folder = '/' + options.folder; // Game requires folders to have a leading slash. Add one if it's missing.
+    if (options.folder && !options.folder.endsWith('/'))
+        options.folder = options.folder + '/';
+    
+    for (let file of ns.ls('home', options.folder))
+        //ns.tprint(`prepare deleting ${file}`);
         ns.rm(file);
 }
